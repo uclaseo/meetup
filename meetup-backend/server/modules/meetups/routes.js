a2374aabdf3b60734643cce208c7d0119151539f
inseok <inseok.ucla@gmail.com>
2017-12-18 13:53:26 -0800

    create getall controller

--- meetup-backend/server/modules/meetups/routes.js
+++ meetup-backend/server/modules/meetups/routes.js
@@ -4,5 +4,5 @@ import * as MeetupController from './controller';
 const routes = new Router();
 
 routes.post('/meetups', MeetupController.createMeetup);
-
+routes.get('/meetups', MeetupController.getAllMeetups);
 export default routes;
