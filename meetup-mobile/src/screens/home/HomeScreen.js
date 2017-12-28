6e53a63879b74c248261ad29e9cd0af60dd891c1
inseok <inseok.ucla@gmail.com>
2017-12-28 14:58:53 -0800

    add loading screen

--- meetup-mobile/src/screens/home/HomeScreen.js
+++ meetup-mobile/src/screens/home/HomeScreen.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { MeetupApi } from '../../../constants/api';
+import { LoadingScreen } from '../../commons';
 import styles from './styles/HomeScreen';
 
+const meetupApi = new MeetupApi();
+
 class HomeScreen extends Component {
-  state = { }
+  static defaultProps = {
+    meetupApi,
+  }
+  state = {
+    loading: false,
+    meetups: [],
+  }
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const meetups = await this.props.meetupApi.fetchGroupMeetups();
+    setTimeout(() => this.setState({ loading: false, meetups }), 500);
+  }
+
   render() {
+    if (this.state.loading) {
+      return <LoadingScreen />;
+    }
     return (
       <View style={styles.root}>
         <Text>HomeScreen</Text>
